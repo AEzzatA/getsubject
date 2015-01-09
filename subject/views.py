c5589475940607cfc7ef3df79f0e39f4218ce555
Ahmed Ezzat <AEzzatA@gmail.com>
2015-01-09 11:54:44 +0200

    fixed indention, removed db.sqlite3 from git and added basic template'

--- subject/views.py
+++ subject/views.py
@@ -11,15 +11,18 @@ def search(request):
 			sentence = POSTagger(form.cleaned_data['sentence'])
 			sentence.processText()
 			output = sentence.getSubject()
-			return render(request, 'subject/search.html', {'form': form, 'output': output})
+			return render(request, 'subject/search.html',
+				{'form': form, 'output': output})
 		else:
 			#return with *this field is required error
-			return render(request, 'subject/search.html', {'form': form, 'output': output})
+			return render(request, 'subject/search.html',
+				{'form': form, 'output': output})
 	else:
 		#METHOD == GET
 		#this asumes the browser have opened the page for the first time
 		form = SubjectForm()
-		return render(request, 'subject/search.html', {'form': form, 'output': output})
+		return render(request, 'subject/search.html',
+			{'form': form, 'output': output})
 
 
 
@@ -30,10 +33,11 @@ def search(request):
 		q = request.GET['q']
 		if not q:
 			error = True
-		else: 
+		else:
 			message = 'You have searched for {0}'.format(request.GET['q'])
 			return render(request, 'subject/search.html', {'output': message})
 	else:
 		message = 'You have submitted an empty form'
-		return render(request, 'subject/search.html', {'output': message, 'error': error})
+		return render(request, 'subject/search.html',
+			{'output': message, 'error': error})
 '''
