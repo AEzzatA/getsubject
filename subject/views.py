4883f38679aa11ed101bfdd239c419db59aa2936
Ahmed Ezzat <AEzzatA@gmail.com>
2015-01-11 23:22:52 +0200

    Refactored template to be more DRY for the introduction of an About Page and possibly more

--- subject/views.py
+++ subject/views.py
@@ -10,27 +10,10 @@ def search(request):
 		sentence.processText()
 		output = sentence.getSubject()
 		output = [str(x) for x in output]
-		return render(request, 'subject/search.html',
+		return render(request, 'subject/home.html',
 			{'text': form, 'output': output})
 
 	else:
-		return render(request, 'subject/search.html',
+		return render(request, 'subject/home.html',
 			{'output': output,})
 
-
-
-'''
-def search(request):
-	error = False
-	if 'q' in request.GET:
-		q = request.GET['q']
-		if not q:
-			error = True
-		else:
-			message = 'You have searched for {0}'.format(request.GET['q'])
-			return render(request, 'subject/search.html', {'output': message})
-	else:
-		message = 'You have submitted an empty form'
-		return render(request, 'subject/search.html',
-			{'output': message, 'error': error})
-'''
