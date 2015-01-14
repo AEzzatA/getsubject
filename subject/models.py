59e884962aa17e88d3e76a30168f1612e81f80ce
Ahmed Ezzat <AEzzatA@gmail.com>
2015-01-14 23:26:40 +0200

    Created a model to save input data for future test cases

--- subject/models.py
+++ subject/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils import timezone
+
 #The applicatio doesn't really need any modes since
 #It analyzes text on the fly
+
+class Text(models.Model):
+	''' A model to store to text used by users to be analyzed '''
+	text = models.TextField()
+	created_at = models.DateTimeField(default=timezone.now)
+
+	class Meta:
+		ordering = ['-created_at']
+
+	def __unicode__(self):
+		return "{0}".format(self.text, self.created_at)
--- subject/views.py
+++ subject/views.py
@@ -1,11 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from .analyzer import POSTagger
+from .models import Text
 
 def search(request):
 	output = []
 	if request.method == 'POST':
 		form = request.POST['item_text']
+		#Save the sentence to the model for future test data
+		s = Text.objects.create(text=form)
+		s.save()
 		sentence = POSTagger(form)
 		sentence.processText()
 		output = sentence.getSubject()
